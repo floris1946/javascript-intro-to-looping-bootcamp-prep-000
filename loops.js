d12be99abd768665dc8e83caae4eb6cd9a18f031
Florisvan der Breggen <florisvanderbreggen@gmail.com>
2018-03-24 11:09:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ array = {};
 function forLoop(array) {
 	for (let i = 1; i <= 25; i++) {
 		if (i == 1) {
-			console.log(`I am + ${i} strange loop.`);
+			console.log(`I am ${i} strange loop.`);
 		} else {
 			console.log(`I am ${i} strange loops.`);
 		}
